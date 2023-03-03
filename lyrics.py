1a21397f1ef02efb243ca9975e6a515cdefb8857
Natalie <106994676+sterhn@users.noreply.github.com>
2023-03-04 01:07:31 +0300

    Add files via upload

--- lyrics.py
+++ lyrics.py
@@ -4,7 +4,6 @@ import requests
 from dotenv import load_dotenv
 from telebot import types
 
-# getting token and headers from env file
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 HEADERS = {
@@ -17,7 +16,7 @@ def find_artist(artist):
 	url = 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/'
 	querystring = {'q':artist}
 	headers = HEADERS
-	response = requests.request('GET', url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=HEADERS, params=querystring)
 	answer= response.json()['sections'][3]['hits']
 	if not answer:
 		raise Exception
@@ -31,25 +30,22 @@ def find_artist(artist):
 def find_song(song):
 	url = 'https://genius-song-lyrics1.p.rapidapi.com/search/'
 	querystring = {'q':song}
-	headers = HEADERS
-	response = requests.request('GET', url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=HEADERS, params=querystring)
 	response = response.json()['hits'][0]['result']
-	return response['id'], response['full_title']
+	return response['id'], response['full_title'], response['url']
 # artist description just because
 def artist_details(id):
 	url = 'https://genius-song-lyrics1.p.rapidapi.com/artist/details/'
 	querystring = {'id':id, 'text_format':'plain'}
-	headers = HEADERS
-	response = requests.request('GET', url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=HEADERS, params=querystring)
 	return response.json()['artist']['description']['plain']
 # searching for lyrics
-def song_lyrics(song):
+def song_lyrics(song_id):
 	url = 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/'
-	song_id, title = find_song(song)
 	querystring = {'id':song_id, 'text_format':'plain'}
-	headers = HEADERS
-	response = requests.request('GET', url, headers=headers, params=querystring)
-	return title, response.json()['lyrics']['lyrics']['body']['plain']
+	response = requests.request('GET', url, headers=HEADERS, params=querystring)
+	lyrics = response.json()['lyrics']['lyrics']['body']['plain']
+	return lyrics
 
 # ------------------------BOT -------------------------
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -67,23 +63,23 @@ def artist_desc(message):
 		bot.send_message(message.chat.id, text= '<i> '+ desc + ' </>', parse_mode='html')
 		bot.send_photo(message.chat.id, photo)
 	except Exception as e:
-		bot.send_message(message.chat.id, 'Can\'t find this artis. Can you try again?')	
+		bot.send_message(message.chat.id, 'Can\'t find this artist. Can you try again?')	
 		bot.send_photo(message.chat.id,'https://i.pinimg.com/474x/5c/09/ab/5c09ab5bc07ef08b98e402572939db4d.jpg')
 
-# searching for song after command
-@bot.message_handler(commands=['song'])
-def lyrics(message):
-	msg = bot.send_message(message.chat.id, text = '🎼 type song name (use artist name for better result)')
-	bot.register_next_step_handler(msg, get_text)
-# just title and lyrics (because if artist is not specified are similar can give wrong results)
-def get_text(message):
-	try:
-		title, lyrics = song_lyrics(message.text)
+# searching for song 
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+	if message.text:
+		song_id, title, link = find_song(message.text)
+		lyrics = song_lyrics(song_id)
 		bot.send_message(message.chat.id, text= '<b> 🎤 '+ title + ' </>', parse_mode='html')
-		bot.send_message(message.chat.id, text= '<i> '+ lyrics + ' </>', parse_mode='html')
-	except Exception as e:
-		bot.send_message(message.chat.id, 'Can\'t find this song. Can you try again?')	
-		bot.send_photo(message.chat.id,'https://i.pinimg.com/474x/5c/09/ab/5c09ab5bc07ef08b98e402572939db4d.jpg')
+		if len(lyrics)>4095:
+			part = lyrics[:4095].rfind('\n\n')
+			bot.send_message(message.chat.id, text= '<i> '+ lyrics[:part] + ' </>', parse_mode='html')
+			bot.send_message(message.chat.id, text= '<i> '+ lyrics[part:] + ' </>', parse_mode='html')
+		else:
+			bot.send_message(message.chat.id, text= '<i> '+ lyrics + ' </>', parse_mode='html')
+		bot.send_message(message.chat.id, text = f'[✨]({link})', parse_mode='MarkdownV2')
 
 # Enable saving next step handlers to file './.handlers-saves/step.save'.
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
