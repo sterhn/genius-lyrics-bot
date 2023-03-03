c09a535f41f8aa05141ce8b7d326ac4ba95d0caf
Natalie <106994676+sterhn@users.noreply.github.com>
2023-03-03 13:24:39 +0300

    Add files via upload

--- lyrics.py
+++ lyrics.py
@@ -4,20 +4,20 @@ import requests
 from dotenv import load_dotenv
 from telebot import types
 
-# loading bot token with env
+# загружаем токен через енв файл
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 HEADERS = {
-		"X-RapidAPI-Key": "17f1658691mshb4eaf044408eeadp198e83jsnfb23894bd828",
-		"X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
+		'X-RapidAPI-Key': os.environ.get('KEY'),
+		'X-RapidAPI-Host': os.environ.get('HOST')
 	}
 # all the search music function
 # looking for artist - searching for id, getting description from this id and cover photo
 def find_artist(artist):
-	url = "https://genius-song-lyrics1.p.rapidapi.com/search/multi/"
-	querystring = {"q":artist}
+	url = 'https://genius-song-lyrics1.p.rapidapi.com/search/multi/'
+	querystring = {'q':artist}
 	headers = HEADERS
-	response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=headers, params=querystring)
 	answer= response.json()['sections'][3]['hits']
 	if not answer:
 		raise Exception
@@ -29,26 +29,26 @@ def find_artist(artist):
 	return title, description, cover_photo
 # looking for song id
 def find_song(song):
-	url = "https://genius-song-lyrics1.p.rapidapi.com/search/"
-	querystring = {"q":song}
+	url = 'https://genius-song-lyrics1.p.rapidapi.com/search/'
+	querystring = {'q':song}
 	headers = HEADERS
-	response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=headers, params=querystring)
 	response = response.json()['hits'][0]['result']
 	return response['id'], response['full_title']
 # artist description just because
 def artist_details(id):
-	url = "https://genius-song-lyrics1.p.rapidapi.com/artist/details/"
-	querystring = {"id":id, "text_format":"plain"}
+	url = 'https://genius-song-lyrics1.p.rapidapi.com/artist/details/'
+	querystring = {'id':id, 'text_format':'plain'}
 	headers = HEADERS
-	response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=headers, params=querystring)
 	return response.json()['artist']['description']['plain']
 # searching for lyrics
 def song_lyrics(song):
-	url = "https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/"
+	url = 'https://genius-song-lyrics1.p.rapidapi.com/song/lyrics/'
 	song_id, title = find_song(song)
-	querystring = {"id":song_id, "text_format":"plain"}
+	querystring = {'id':song_id, 'text_format':'plain'}
 	headers = HEADERS
-	response = requests.request("GET", url, headers=headers, params=querystring)
+	response = requests.request('GET', url, headers=headers, params=querystring)
 	return title, response.json()['lyrics']['lyrics']['body']['plain']
 
 # ------------------------BOT -------------------------
@@ -85,12 +85,12 @@ def get_text(message):
 		bot.send_message(message.chat.id, 'Can\'t find this song. Can you try again?')	
 		bot.send_photo(message.chat.id,'https://i.pinimg.com/474x/5c/09/ab/5c09ab5bc07ef08b98e402572939db4d.jpg')
 
-# Enable saving next step handlers to file "./.handlers-saves/step.save".
+# Enable saving next step handlers to file './.handlers-saves/step.save'.
 # Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
 # saving will hapen after delay 2 seconds.
 bot.enable_save_next_step_handlers(delay=2)
 
-# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
+# Load next_step_handlers from save file (default './.handlers-saves/step.save')
 # WARNING It will work only if enable_save_next_step_handlers was called!
 bot.load_next_step_handlers()	
 bot.infinity_polling()
